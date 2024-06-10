e0f18d967710da36e31f6949fbb7b9527ec580ad
Sarvar Sultanov <iamsarvarsultanov@gmail.com>
2024-06-11 00:25:27 +0500

    connected to mongoDB, user and auth routes created

--- app.js
+++ app.js
@@ -3,20 +3,27 @@ const cors = require("cors");
 const dotenv = require("dotenv");
 const helmet = require("helmet");
 const morgan = require("morgan");
+const userRouter = require("./routes/userRoutes");
+const authRouter = require("./routes/authRoutes");
 
 dotenv.config();
 
 const app = express();
 
+// Middlewares
 app.use(express.json());
 app.use(cors());
 app.use(helmet());
 app.use(morgan("common"));
 
-app.get("/", (req, res) => {
-  res.status(200).json({
-    status: "success",
-    message: "Hello from server!",
+// Routes
+app.use("/api/v1/users", userRouter);
+app.use("/api/v1/auth", authRouter);
+
+app.get("*", (req, res) => {
+  res.status(404).json({
+    status: "fail",
+    message: "Page not found!",
   });
 });
 
--- server.js
+++ server.js
@@ -2,6 +2,16 @@ const mongoose = require("mongoose");
 const app = require("./app");
 const port = 8000;
 
+const DB = process.env.DATABASE;
+
+mongoose.connect(
+  DB,
+  { useNewUrlParser: true, useUnifiedTopology: true },
+  () => {
+    console.log("Connected successfully");
+  }
+);
+
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
